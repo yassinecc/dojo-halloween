d122988997b0025c5500e2e1701e21f41545a68a
Yassine Chbani <yassinec@bam.tech>
2018-09-23 12:04:27 +0200

    Add PanResponder to image

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet, Image, View, PanResponder } from 'react-native';
 import backgroundImage from './src/images/background.jpg';
 
 export default class App extends React.Component {
+  state = {
+    initial: {
+      x: 0,
+      y: 0,
+    },
+    delta: {
+      x: 0,
+      y: 0,
+    },
+  };
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, gestureState) => {
+      this.setState({ delta: { x: gestureState.dx, y: gestureState.dy } });
+    },
+    onPanResponderRelease: () => {
+      const { initial, delta } = this.state;
+      this.setState({
+        initial: { x: initial.x + delta.x, y: initial.y + delta.y },
+
+        delta: { x: 0, y: 0 },
+      });
+    },
+  });
+
   render() {
+    const { initial, delta } = this.state;
+    const imageStyle = {
+      left: initial.x + delta.x,
+      top: initial.y + delta.y,
+      resizeMode: 'contain',
+    };
     return (
       <View style={styles.container}>
-        <Image source={backgroundImage} style={styles.image} />>
+        <Image source={backgroundImage} style={imageStyle} {...this.panResponder.panHandlers} />
       </View>
     );
   }
@@ -18,10 +49,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  image: {
-    left: 0,
-    top: 0,
-    resizeMode: 'contain',
   },
 });
