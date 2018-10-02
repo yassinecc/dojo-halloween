86bfa60568849dc08465c5acc964f26e143087ba
Yassine Chbani <yassinec@bam.tech>
2018-10-02 13:44:25 +0200

    Add character sprites

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,7 @@ export { default as backgroundImage } from './background.jpg';
 export { default as full_heart } from './full_heart.png';
 export { default as empty_heart } from './empty_heart.png';
 export { default as slenderMan } from './slenderman.jpg';
+export { default as characterDown } from './down.png';
+export { default as characterUp } from './up.png';
+export { default as characterLeft } from './left.png';
+export { default as characterRight } from './right.png';
